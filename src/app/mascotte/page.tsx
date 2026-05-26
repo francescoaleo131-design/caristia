5b781d86a1bc6d40a8387df9951a039023257762
Francesco <francescoaleo131@gmail.com>
2026-05-26 21:02:10 +0200

    immagini

--- src/app/mascotte/page.tsx
+++ src/app/mascotte/page.tsx
@@ -71,6 +71,14 @@ export default function MascottePage() {
                 />
             )}
 
+             <div className="w-full my-12 px-4 container mx-auto">
+          <img 
+            src="/divider_4.png" 
+            alt="Promozione Giocattoli"
+            className="w-full h-auto"
+            style={{ maxHeight: "400px" }}
+          />
+        </div>
             {/* Footer Divider */}
             <div className="w-full my-12 px-4 container mx-auto">
                 <div className="h-px bg-slate-100 w-full" />
