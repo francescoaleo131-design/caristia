a0a77f393fa1e3491c776f8384783ce751a400d5
Francesco <francescoaleo131@gmail.com>
2026-06-07 01:58:30 +0200

    miglioramento estetico

--- src/lib/loops/loops.ts
+++ src/lib/loops/loops.ts
@@ -4,5 +4,4 @@ if (!process.env.LOOPS_API_KEY) {
   throw new Error("Manca la variabile d'ambiente LOOPS_API_KEY");
 }
 
-// Inizializziamo il client unico da esportare ovunque nel backend
 export const loops = new LoopsClient(process.env.LOOPS_API_KEY);
